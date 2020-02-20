77c014d955740b799c0fe60f0f84720153040636
Doak Covington <doakcovington@protonmail.com>
2020-02-20 02:31:27 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -32,4 +32,31 @@ class Waiter
 
     best_tipped_meal.customer
   end
+
+  def customers
+    customers = []
+    Meal.all.each do |meals|
+      customers << meals.customer.name
+    end
+    customers
+  end
+
+  def most_frequent
+    customers.max_by {|customer| customers.count(customer)}
+  end
+
+  def worst_tipped_meal
+    worst_meal = meals.min do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    worst_meal
+  end
+
+  def self.least_experienced
+    waiter = Waiter.all.min do |waiter_a, waiter_b|
+      waiter_a.yrs_experience <=> waiter_b.yrs_experience
+    end
+    waiter
+  end
+
 end
